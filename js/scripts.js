338e240d6d939fd36c323ce6cc5c218ab309c2dd
dusty mccord and josh Hellman <josh@>
2020-01-23 09:52:34 -0800

    added front end Jquery to get page to function properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
     function isLeapYear(year) {
         if ((year % 4 === 0) && (year % 100) || (year % 400 === 0)) {
-            return true;
+            return "true";
         } else {
-            return false;
+            return "false";
         }
     }
-
-    var yearOption1 = isLeapYear(1993);
-    var yearOption2 = isLeapYear(2004);
-    var yearOption3 = isLeapYear(1900);
-    var yearOption4 = isLeapYear(2000);
-    var yearOption5 = isLeapYear(2020);
-
- console.log(yearOption1, yearOption2, yearOption3, yearOption4, yearOption5)
+    $("#submitBtn").click(function(event) {
+        event.preventDefault();
+        var year = parseInt($("#year").val());
+        var checkYear = isLeapYear(year)
+        $("#results").text(checkYear)
+        console.log(checkYear)
+    })
 });
 
